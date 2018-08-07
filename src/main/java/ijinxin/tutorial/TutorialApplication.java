a083bf6f480c52458b4290f7335b6fe28df0d6ef
ijinxin <874450097@qq.com>
2018-08-07 10:37:08 +0800

    simple jpa query

--- src/main/java/ijinxin/tutorial/TutorialApplication.java
+++ src/main/java/ijinxin/tutorial/TutorialApplication.java
@@ -2,11 +2,27 @@ package ijinxin.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TutorialApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TutorialApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(AccountRepository accountRepository,
+                           BookmarkRepository bookmarkRepository) {
+        return args -> Arrays.asList("dsda", "dada", "jinxin", "ijinxin", "asdf")
+                .forEach(username -> {
+                    Account account = accountRepository.save(new Account(username, "password"));
+                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + username, "A description"));
+                    bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + username, "A description"));
+                });
     }
 }
